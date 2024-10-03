9b73a5f57b350a32bbcd1d6f541f338ed939f7be
zoya5636 <zoya@chishtie.com>
2024-10-03 14:36:02 -0400

    Completed Activity 11 - Login Required

--- project/app.py
+++ project/app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
 
 
 basedir = Path(__file__).resolve().parent
@@ -26,6 +27,15 @@ db = SQLAlchemy(app)
 
 from project import models
 
+def login_required(f):
+    print(f)
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
 def index():
@@ -69,13 +79,14 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('index'))
 
-
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db.session.query(models.Post).filter_by(id=post_id).delete()
+        new_id = post_id
+        db.session.query(models.Post).filter_by(id=new_id).delete()
         db.session.commit()
         result = {'status': 1, 'message': "Post Deleted"}
         flash('The entry was deleted.')
--- tests/app_test.py
+++ tests/app_test.py
@@ -3,7 +3,7 @@ import pytest
 from pathlib import Path
 import json
 
-from project.app import app, db
+from project.app import app, db, login_required
 
 TEST_DB = "test.db"
 
@@ -83,6 +83,18 @@ def test_search(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+def test_login_required():
+    with app.test_request_context():
+        @login_required
+        def sample_function():
+            return jsonify({'testKey1': 0}), 401
+        response = sample_function()
+        assert response[1] == 401
